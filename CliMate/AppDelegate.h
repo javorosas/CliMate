9c7217f5d343aa8c94f4580c07e50de5caab7a9f
Javier Rosas <jrosas@Javos-Mac.local>
2015-05-07 15:31:38 -0700

    Initial Commit

---
+++ CliMate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CliMate
+//
+//  Created by Javier Rosas on 5/7/15.
+//  Copyright (c) 2015 Javier Rosas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
